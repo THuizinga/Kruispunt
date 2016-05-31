7fc90cddaddc02ae0394349eab3b538ebb673241
Tiko Huizinga <thuizinga@cs.ru.nl>
2016-05-31 23:21:37 +0200

    Opdracht 1 en 2 gemaakt. Zone gemaakt. Auto moet nog de run() gemaakt worden.

--- src/kruispunt/Main.java
+++ src/kruispunt/Main.java
@@ -6,13 +6,42 @@ package kruispunt;
  * @author Conny Blach - s4329872
 */
 
-public class Main {
+/**
+ * We hebben auto's a0..a3 en zones z0..z3
+ * Opdracht1:
+ * Stel a0 wil links en a2 wil links. 
+ * Als ze allebei eerst hun dichtsbijzijnde zone reserveren (z0 en z2 resp.),
+ * daarna de zone voor rechtdoor (z1 en z3 resp.)
+ * en daarna de zone het verst van zich af willen reserveren (z2 en z0 resp.),
+ * dan treedt er een deadlock op omdat z2 al gereserveerd is door a2 en
+ * z0 al gereserveerd voor a0. Nu zullen de auto's oneindig lang op elkaar
+ * wachten. Dit is een deadlock.
+ * 
+ * Opdracht 2:
+ * Als de auto's de zones reserveren op volgorde van hun ID (0,1,2,3), van klein
+ * naar groot, dan zal dit niet optreden.
+ */
 
+public class Main {
+    
+    public Main(){
+        Zone z0 = new Zone(0);
+        Zone z1 = new Zone(1);
+        Zone z2 = new Zone(2);
+        Zone z3 = new Zone(3);
+        
+        Auto a0 = new Auto(0, z0, z1, z2);
+        Auto a1 = new Auto(0, z1, z2, z3);
+        Auto a2 = new Auto(0, z2, z3, z0);
+        Auto a3 = new Auto(0, z3, z0, z1);
+    }
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Main();
     }
     
 }
