2f6bc2b8a4f6aeb5a5220eb18d4f1bade925ce92
Tiko Huizinga <thuizinga@cs.ru.nl>
2016-06-01 23:26:08 +0200

    Formatting verbeterd via netBeans auto format

--- src/kruispunt/Auto.java
+++ src/kruispunt/Auto.java
@@ -8,81 +8,82 @@ import java.util.logging.Logger;
 /**
  * @author Tiko Huizinga - s4460898
  * @author Conny Blach - s4329872
-*/
-public class Auto implements Runnable{
+ */
+public class Auto implements Runnable {
+
     private final int ID;
     private Random r1;
-    private Zone [] zones;
-    
-    
+    private Zone[] zones;
+
     /**
      * Maakt een auto aan en zet de zones en ID goed.
+     *
      * @param id
      * @param rechts
      * @param rechtdoor
-     * @param links 
+     * @param links
      */
-    public Auto(int id, Zone rechts, Zone rechtdoor, Zone links){
+    public Auto(int id, Zone rechts, Zone rechtdoor, Zone links) {
         ID = id;
         r1 = new Random();
-        zones = new Zone [] {rechts, rechtdoor, links};
-        
+        zones = new Zone[]{rechts, rechtdoor, links};
+
     }
-    
+
     /**
      * Geef een willekeurig getal tussen 0 en 2 die de richting voorstelt
-     * @return 
+     *
+     * @return
      */
-    private int geefRichting(){
+    private int geefRichting() {
         return r1.nextInt(3);
     }
-    
-    private void printGebeurtenis(int i){
-        if(i ==0)
-            System.out.println("Auto " + this + " is rechtsaf gegaan");
-        else if(i == 1)
-            System.out.println("Auto " + this + " is rechtdoor gegaan");
-        else
-            System.out.println("Auto " + this + " is linksaf gegaan");
 
+    private void printGebeurtenis(int i) {
+        if (i == 0) {
+            System.out.println("Auto " + this + " is rechtsaf gegaan");
+        } else if (i == 1) {
+            System.out.println("Auto " + this + " is rechtdoor gegaan");
+        } else {
+            System.out.println("Auto " + this + " is linksaf gegaan");
+        }
 
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return String.valueOf(ID);
     }
-    
 
     /**
      * De zone's die gereserveerd moeten worden worden in een array gezet en
-     * gesorteerd op hun ID.
-     * De auto begint met 1 tot 2 seconde rondjes rijden en komt dan bij het 
-     * kruispunt aan. Elke zone wordt om de beurt gereserveerd.
-     * Dan wordt elke zone om de beurt weer vrijgegeven en wordt er geprint
-     * welke richting de auto op is gegaan. Daarna begint dit weer opnieuw.
+     * gesorteerd op hun ID. De auto begint met 1 tot 2 seconde rondjes rijden
+     * en komt dan bij het kruispunt aan. Elke zone wordt om de beurt
+     * gereserveerd. Dan wordt elke zone om de beurt weer vrijgegeven en wordt
+     * er geprint welke richting de auto op is gegaan. Daarna begint dit weer
+     * opnieuw.
      */
     @Override
     public void run() {
-        while(true){
+        while (true) {
             try {
                 int richting = geefRichting();
-                Zone [] nodig = Arrays.copyOf(zones, richting+1);
+                Zone[] nodig = Arrays.copyOf(zones, richting + 1);
                 Arrays.sort(nodig);
-                Thread.sleep(r1.nextInt(1000) + 1000);
-                for(int i =0; i<=richting; i++ ){
+                Thread.sleep(r1.nextInt(3000) + 3000);
+                for (int i = 0; i <= richting; i++) {
                     nodig[i].rijOp();
-                    //System.out.println("Auto " + this + " reserveert: " + nodig[i]);
+                    System.out.println("Auto " + this + " reserveert: " + nodig[i]);
                 }
-                for(int i =0; i<=richting; i++ ){
+                for (int i = 0; i <= richting; i++) {
                     nodig[i].rijAf();
                 }
                 printGebeurtenis(richting);
             } catch (InterruptedException ex) {
                 System.out.println("Sleep interrupted..");
             }
-                        
+
         }
     }
-    
+
 }
--- src/kruispunt/Main.java
+++ src/kruispunt/Main.java
@@ -1,69 +1,65 @@
-
 package kruispunt;
 
 /**
  * @author Tiko Huizinga - s4460898
  * @author Conny Blach - s4329872
-*/
-
-/**
- * We hebben auto's a0..a3 en zones z0..z3
- * Opdracht1:
- * Stel a0 wil links en a2 wil links. 
- * Als ze allebei eerst hun dichtsbijzijnde zone reserveren (z0 en z2 resp.),
- * daarna de zone voor rechtdoor (z1 en z3 resp.)
- * en daarna de zone het verst van zich af willen reserveren (z2 en z0 resp.),
- * dan treedt er een deadlock op omdat z2 al gereserveerd is door a2 en
- * z0 al gereserveerd voor a0. Nu zullen de auto's oneindig lang op elkaar
- * wachten. Dit is een deadlock.
- * 
- * Opdracht 2:
- * Als de auto's de zones reserveren op volgorde van hun ID (0,1,2,3), van klein
- * naar groot, dan zal dit niet optreden.
  */
-
+/**
+ * We hebben auto's a0..a3 en zones z0..z3 Opdracht1: Stel a0 wil links en a2
+ * wil links. Als ze allebei eerst hun dichtsbijzijnde zone reserveren (z0 en z2
+ * resp.), daarna de zone voor rechtdoor (z1 en z3 resp.) en daarna de zone het
+ * verst van zich af willen reserveren (z2 en z0 resp.), dan treedt er een
+ * deadlock op omdat z2 al gereserveerd is door a2 en z0 al gereserveerd voor
+ * a0. Nu zullen de auto's oneindig lang op elkaar wachten. Dit is een deadlock.
+ *
+ * Opdracht 2: Als de auto's de zones reserveren op volgorde van hun ID
+ * (0,1,2,3), van klein naar groot, dan zal dit niet optreden.
+ */
 public class Main {
+
     Thread t1, t2, t3, t4;
+
     /**
-     * Hier worden alle auto's zones en threads aangemaakt.
-     * Daarna wordt de simulatie gestart.
+     * Hier worden alle auto's zones en threads aangemaakt. Daarna wordt de
+     * simulatie gestart.
      */
-    public Main(){
+    public Main() {
         Zone z0 = new Zone(0);
         Zone z1 = new Zone(1);
         Zone z2 = new Zone(2);
         Zone z3 = new Zone(3);
-        
+
         Auto a0 = new Auto(0, z0, z1, z2);
         Auto a1 = new Auto(1, z1, z2, z3);
         Auto a2 = new Auto(2, z2, z3, z0);
         Auto a3 = new Auto(3, z3, z0, z1);
-        
+
         t1 = new Thread(a0);
         t2 = new Thread(a1);
         t3 = new Thread(a2);
         t4 = new Thread(a3);
-        
+
         startSim();
-        
+
     }
-    
+
     /**
-     * Start de simulatie. Dit gebeurt niet in de constructor omdat 
-     * dat gevaarlijk is.
+     * Start de simulatie. Dit gebeurt niet in de constructor omdat dat
+     * gevaarlijk is.
      */
-    private void startSim(){
+    private void startSim() {
         t1.start();
         t2.start();
         t3.start();
         t4.start();
 
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         new Main();
     }
-    
+
 }
