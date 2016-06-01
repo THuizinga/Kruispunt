42b7f5ed86d9dbbf79c6576dd281e7acb31a1c41
Tiko Huizinga <thuizinga@cs.ru.nl>
2016-06-01 23:16:30 +0200

    Javadoc en printGebeurtenis() toegevoegd. Run gemaakt en af.

--- src/kruispunt/Auto.java
+++ src/kruispunt/Auto.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kruispunt;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,7 +15,13 @@ public class Auto implements Runnable{
     private Zone [] zones;
     
     
-    
+    /**
+     * Maakt een auto aan en zet de zones en ID goed.
+     * @param id
+     * @param rechts
+     * @param rechtdoor
+     * @param links 
+     */
     public Auto(int id, Zone rechts, Zone rechtdoor, Zone links){
         ID = id;
         r1 = new Random();
@@ -27,16 +29,59 @@ public class Auto implements Runnable{
         
     }
     
+    /**
+     * Geef een willekeurig getal tussen 0 en 2 die de richting voorstelt
+     * @return 
+     */
     private int geefRichting(){
-        System.out.println(r1.nextInt(3));
         return r1.nextInt(3);
     }
     
+    private void printGebeurtenis(int i){
+        if(i ==0)
+            System.out.println("Auto " + this + " is rechtsaf gegaan");
+        else if(i == 1)
+            System.out.println("Auto " + this + " is rechtdoor gegaan");
+        else
+            System.out.println("Auto " + this + " is linksaf gegaan");
 
+
+    }
+    
+    @Override
+    public String toString(){
+        return String.valueOf(ID);
+    }
+    
+
+    /**
+     * De zone's die gereserveerd moeten worden worden in een array gezet en
+     * gesorteerd op hun ID.
+     * De auto begint met 1 tot 2 seconde rondjes rijden en komt dan bij het 
+     * kruispunt aan. Elke zone wordt om de beurt gereserveerd.
+     * Dan wordt elke zone om de beurt weer vrijgegeven en wordt er geprint
+     * welke richting de auto op is gegaan. Daarna begint dit weer opnieuw.
+     */
     @Override
     public void run() {
         while(true){
-            //TODO run maken
+            try {
+                int richting = geefRichting();
+                Zone [] nodig = Arrays.copyOf(zones, richting+1);
+                Arrays.sort(nodig);
+                Thread.sleep(r1.nextInt(1000) + 1000);
+                for(int i =0; i<=richting; i++ ){
+                    nodig[i].rijOp();
+                    //System.out.println("Auto " + this + " reserveert: " + nodig[i]);
+                }
+                for(int i =0; i<=richting; i++ ){
+                    nodig[i].rijAf();
+                }
+                printGebeurtenis(richting);
+            } catch (InterruptedException ex) {
+                System.out.println("Sleep interrupted..");
+            }
+                        
         }
     }
     
