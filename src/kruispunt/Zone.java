3e8731cf2d2ed550c3a848f1fcc21113bf376f40
Tiko Huizinga <thuizinga@cs.ru.nl>
2016-06-01 23:14:25 +0200

    CompareTo en toString aangemaakt om vanuit Auto te kunnen gebruiken. Javadoc verbeterd.

--- src/kruispunt/Zone.java
+++ src/kruispunt/Zone.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kruispunt;
 
 import java.util.logging.Level;
@@ -12,19 +7,20 @@ import java.util.logging.Logger;
  * @author Tiko Huizinga - s4460898
  * @author Conny Blach - s4329872
 */
-public class Zone {
+public class Zone implements Comparable<Zone>{
     private final int ID;
-    private Boolean bezet;
+    private Boolean bezet = false;
     
     public Zone(int zoneID){
         ID = zoneID;
         
     }
     /**
-     * Maakt de zone bezet zodat andere auto's weten dat ze hem niet kunnen
-     * reserveren.
+     * Zolang de zone bezet is, moet de thread wachten. 
+     * Daarna maakt het de zone bezet zodat andere auto's weten dat ze hem niet
+     * kunnen reserveren.
      */
-    public synchronized void rijOp(){
+    public synchronized void rijOp() {
         while(bezet){
             try {
                 wait();
@@ -39,6 +35,7 @@ public class Zone {
     
     /**
      * Geeft de zone weer vrij zodat andere auto's de zone kunnen reserveren.
+     * Laat alle threads weten dat de zone vrijgegeven is.
      */
     public synchronized void rijAf(){
         bezet = false;
@@ -51,6 +48,26 @@ public class Zone {
     
     public int getID(){
         return this.ID;
+    }
+    
+    @Override
+    public String toString(){
+        return String.valueOf(ID);
+    }
+    
+    /**
+     * Vergelijkt deze zone met zone z op basis van ID.
+     * @param z zone waarmee this vergeleken moet worden
+     * @return 
+     */
+    @Override
+    public int compareTo(Zone z) {
+        if(this.ID>z.ID)
+            return 1;
+        else if (this.ID == z.ID)
+            return 0;
+        else
+            return -1;
     }
     
 }
